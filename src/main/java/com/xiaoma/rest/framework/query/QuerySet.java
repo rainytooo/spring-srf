b60898a59099eca8460bbd0f44d0227ed4f235cc
vincent <ohergal@gmail.com>
2016-05-20 22:12:49 +0800

    first init

---
+++ src/main/java/com/xiaoma/rest/framework/query/QuerySet.java
@@ -0,0 +1,12 @@
+package com.xiaoma.rest.framework.query;
+
+/**
+ * 查询对象 将参数重新封装
+ *
+ * @author <a href="mailto:ergal@163.com">vincent.omg</a>
+ * @version 1.0
+ * @date 16/5/20
+ * @since 1.0
+ */
+public class QuerySet {
+}
