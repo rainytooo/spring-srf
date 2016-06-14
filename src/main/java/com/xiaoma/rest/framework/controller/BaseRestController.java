5fe1cb977274259f5cf98ba9f68b778dab226cd3
Vincent <ohergal@gmail.com>
2016-06-14 17:28:13 +0800

    做一个顶级的restcontroller

---
+++ src/main/java/com/xiaoma/rest/framework/controller/BaseRestController.java
@@ -0,0 +1,22 @@
+package com.xiaoma.rest.framework.controller;
+
+import com.xiaoma.rest.framework.page.PaginationParameter;
+import com.xiaoma.rest.framework.query.GenericQuerySet;
+import com.xiaoma.rest.framework.query.QuerySet;
+
+/**
+ * Created by vincent on 16/6/14.
+ */
+public class BaseRestController {
+
+    // 定义controller的queryset类
+    private Class<? extends QuerySet> querySetClass = GenericQuerySet.class;
+
+    // 定义分页配置参数类
+    private Class<? extends PaginationParameter> paginationParameterClass = PaginationParameter.class;
+
+    public BaseRestController() {
+    }
+
+    
+}
